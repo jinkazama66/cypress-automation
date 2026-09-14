35890da38ffa838cccc59259911f796f73d2378d
MacBook Pro <macbookpro@192.168.10.4>
2026-09-14 18:01:19 +0500

    Set up Cypress project for CI

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,22 @@
-describe('SauceDemo Login', () => {
-  it('should login successfully', () => {
+describe('Login to SauceDemo, add backpack to cart, open cart and verify product', () => {
+
+  it('should login, add backpack to cart and display it in cart', () => {
+
     cy.visit('https://www.saucedemo.com/')
 
     cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
 
-    cy.get('.title').should('be.visible')
+    cy.get('#password').type('secret_sauce')
+
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+    cy.get('[data-test="shopping-cart-link"]').click()
+
+    cy.get('[data-test="inventory-item-name"]')
+      .should('have.text', 'Sauce Labs Backpack')
+
   })
+
 })
